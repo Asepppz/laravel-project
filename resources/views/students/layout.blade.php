930b0ef9708f38461dbea29ebfc84f24450c4bf0
unknown <Dioardiyanmaula@gmail.com>
2024-10-14 09:48:31 +0700

    first init github

---
+++ resources/views/students/layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Student Laravel Crud</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootsrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+
+<div class="container">
+    @yield('content')
+</div>
+    
+</body>
+</html>
